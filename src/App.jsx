d62c155718797c7ac0c55b8980ed129fa432fd32
georggeo23 <georgechris2003@gmail.com>
2025-05-07 14:46:47 -0400

    importing styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
-const Card =({title})=>{
-  return (
-    <div style={{
-      border: '1px solid #4b5352',
-      padding: '20px',
-      margin: '10px',
-      backgroundColor: '#31363f',
-      borderRadius: '10px',
-      minHeight: '100px',
-    }}>
-      <h2>{title}</h2>
-      </div>
-  )
-}
+import React from 'react'
 
 const App = () =>{
   return (
-    <div className="card-container">
-      <h2>Functional Arrow Component</h2>
-      <Card title="Star Wars" rating={5} isCool = {true}/>
-      <Card title="Avatar"/>
-      <Card title="The Lion King"/>
+    <div>
+      <h1 className='text-3xl font0bold underline'>
+        Hello world!
+      </h1>
     </div>
   )
 }
