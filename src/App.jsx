d989868f1f76412e98d6f18f78717a91214c6d9f
georggeo23 <georgechris2003@gmail.com>
2025-05-08 09:12:42 -0400

    Search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Search from "./components/Search.jsx";
 
 const App = () =>{
-  return (
-      <main>
-          <div className="pattern"/>
-          <div className="wrapper">
-              <header className="header">
-                  <img src="/hero.png" alt="hero banner"/>
-                  <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
-              </header>
-          </div>
-      </main>
-
-  )
+    const [searchTerm, setSearchTerm] = useState("");
+    return (
+        <main>
+            <div className="pattern"/>
+            <div className="wrapper">
+                <header>
+                    <img src="/hero.png" alt="hero banner"/>
+                    <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+                </header>
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            </div>
+        </main>
+    )
 }
 export default App
