60cf992f2e078b9eebe03f3e2d49be615d0f7bba
georggeo23 <georgechris2003@gmail.com>
2025-05-09 18:47:42 -0400

    amending API endpoints

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React, {useEffect, useState} from 'react'
 import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
+import MovieCard from "./components/MovieCard.jsx";
 
-const API_BASE_URL ='http://api.themoviedb.org/3';
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_BASE_URL ='https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_API_KEY;
 
-const API_OPTIONS={method: 'GET'}
+const API_OPTIONS={
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_KEY}`,
+}}
 
 
 const App = () =>{
@@ -42,7 +48,6 @@ const App = () =>{
     }
     useEffect(()=>{
         fetchMovies();
-
     },[]);
     return (
         <main>
@@ -62,7 +67,7 @@ const App = () =>{
                     ):(
                         <ul>
                             {movieList.map((movie)=>(
-                                <p className={'text-white'}>{movie.title}</p>
+                                <MovieCard key={movie.id} movie={movie}/>
                             ))}
                         </ul>
                     )
